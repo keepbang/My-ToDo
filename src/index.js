d41bdabc79d678ee20851e3c8b7afe4e57a5ed60
keepbang <rlqud1125@gmail.com>
2020-09-26 20:41:03 +0900

    #1 subscribe

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 const countModifier = (count = 0,action) => {
+  console.log(count, action);
   if (action.type === "ADD"){
     return count + 1;
   }else if(action.type === "MINUS"){
@@ -16,3 +17,20 @@ const countModifier = (count = 0,action) => {
 };
 
 const countStore = createStore(countModifier);
+
+const onChange = (v) => {
+  number.innerHTML = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({type:"ADD"});
+}
+
+const handleMinus = () => {
+  countStore.dispatch({type:"MINUS"});
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
