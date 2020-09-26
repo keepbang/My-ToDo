e2c358f71205d5e4129194a95206ecc1050532ad
keepbang <rlqud1125@gmail.com>
2020-09-26 20:34:54 +0900

    #1 createStore

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
+import {createStore} from 'redux';
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const countModifier = (count = 0,action) => {
+  if (action.type === "ADD"){
+    return count + 1;
+  }else if(action.type === "MINUS"){
+    return count - 1;
+  }else{
+    return count;
+  }
 
-const handleAdd = () => {
-  console.log("add");
-}
+};
 
-const handleMinus = () => {
-  console.log("minus");
-}
-
-add.addEventListener("click",handleAdd);
-minus.addEventListener("click",handleMinus);
+const countStore = createStore(countModifier);
