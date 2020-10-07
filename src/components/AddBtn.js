83baa3b553dcde4cdc40e0a7b0817cf08dcb23c3
keepbang <rlqud1125@naver.com>
2020-10-07 16:53:01 +0900

    move btn

--- src/components/AddBtn.js
+++ src/components/AddBtn.js
@@ -1,10 +1,11 @@
+import { Icon, Intent } from '@blueprintjs/core'
 import React from 'react'
 import '../css/AddBtn.scss'
 
 const AddBtn = ({eventProps}) => {
     return <>
         <div className="add_btn" onClick={eventProps}>
-            +
+            <Icon icon="add-to-artifact" iconSize={Icon.SIZE_LARGE} intent={Intent.NONE}/>
         </div>
     </>
 }
